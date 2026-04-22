664c75b7748d18712f5e5145718c29ee98a56267
Nicolas Tribolo <nicolastribolo@MacBook-Air-de-Nicolas.local>
2026-04-22 19:23:38 -0300

    Cambios en el App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+const CHARSETS ={
+  lower: 'abcdefghijklmnopqrstuvwxyz',
+  upper: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
+  numbers: '0123456789',
+  symbols: '!@#$%^&*()_+~`|}{[]:;?><,./-='
+};
+
+function pickRandom(text){
+  return text[Math.floor(Math.random() * text.length)];
+}
+function shuffle(text){
+  const array=text.split('');
+  for (let i=array.length-1; i>0; i--){
+    const j=Math.floor(Math.random() * (i+1));
+    const temp=array[i];
+    array[i]=array[j];
+    array[j]=temp;
+
+  }
+  return array.join('');
+}
+function buildRequirements(password){
+  return[
+    {id: 'len',}
+  ]
+}
 function App() {
   return (
     <div className="App">
